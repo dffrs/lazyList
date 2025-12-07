e09345b1842434134bfb9d0d561f9bfcb667f153
dffrs <dffrs@iscte-iul.pt>
2025-12-07 11:12:23 +0000

    feat: tweaked cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   component: {
+    screenshotOnRunFailure: false,
+    viewportWidth: 1440,
+    viewportHeight: 900,
+    video: false,
+    defaultBrowser: "chrome",
     devServer: {
       framework: "react",
       bundler: "vite",
--- cypress/tests/index.cy.tsx
+++ cypress/tests/index.cy.tsx
@@ -4,5 +4,7 @@ describe("Lazy List test suite", () => {
   it("Lazy List renders", () => {
     // render LazyList component
     cy.mount(<LazyList></LazyList>);
+
+    cy.get('[data-testid="lazy-list-ul"]').should("exist");
   });
 });
