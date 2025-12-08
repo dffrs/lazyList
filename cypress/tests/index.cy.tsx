5d1c91aec9ec359ae6a02ad49ea63cf3860090c7
dffrs <dffrs@iscte-iul.pt>
2025-12-08 14:42:06 +0000

    faet: added second test case

--- cypress/tests/index.cy.tsx
+++ cypress/tests/index.cy.tsx
@@ -26,4 +26,41 @@ describe("Lazy List test suite", () => {
       defaultInitalElements,
     );
   });
+
+  it("renders children until parent is overflowing", () => {
+    const initialElements = 5;
+    const clientHeight = 200;
+
+    cy.mount(
+      <div
+        data-testid="wrapper"
+        style={{ height: clientHeight, overflow: "auto" }}
+      >
+        <LazyList initialElements={initialElements}>{children}</LazyList>
+      </div>,
+    );
+
+    cy.get('[data-testid="lazy-list-ul"]').should("exist");
+
+    // verify that it has more that only 'initialElements'
+    cy.get("[data-testid^='lazy-list-li']").should(
+      "not.have.length",
+      initialElements,
+    );
+
+    // but it should not have all the children also
+    cy.get("[data-testid^='lazy-list-li']").should(
+      "not.have.length",
+      numberOfChildren,
+    );
+
+    cy.get("[data-testid='wrapper']").then(($el) => {
+      const elScrollHeight = $el.prop("scrollHeight");
+      const elClientHeight = $el.prop("clientHeight");
+
+      expect(elClientHeight).to.be.equal(clientHeight);
+
+      expect(elScrollHeight).to.be.greaterThan(elClientHeight);
+    });
+  });
 });
